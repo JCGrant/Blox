f2e5242008a2688de7a56d6cc474ced9506bff81
JCGrant <jamescolin.grant@gmail.com>
2016-04-17 00:33:25 +0100

    Created a simple wrapper around Minecraft's server.

---
+++ main.py
@@ -0,0 +1,56 @@
+import subprocess
+import sys
+from time import strftime
+
+start_command = 'java -jar minecraft_server.1.9.2.jar nogui'
+
+COMMAND = 'COMMAND'
+SHUTDOWN = 'SHUTDOWN'
+
+
+class Wrapper:
+
+    def __init__(self):
+        self.proc = subprocess.Popen(start_command.split(),
+                                     stdout=subprocess.PIPE,
+                                     stdin=subprocess.PIPE,
+                                     stderr=subprocess.PIPE)
+        self.app_name = 'PMCSW'
+        self.command_token = '!'
+
+    def read_from_server(self):
+        data = self.proc.stdout.readline()
+        return data.decode('utf-8').rstrip('\n')
+
+    def send_to_server(self, string):
+        self.proc.stdin.write(bytes(string + '\n', 'utf-8'))
+        self.proc.stdin.flush()
+
+    def handle_command(self, command_text):
+        self.log(command_text, COMMAND)
+        self.send_to_server(command_text)
+
+    def handle_text(self, server_text):
+        print(server_text)
+        player_text = ' '.join(server_text.split()[4:])
+        if player_text.startswith(self.command_token):
+            command_text = player_text.lstrip(self.command_token)
+            self.handle_command(command_text)
+
+    def log(self, message, type):
+        time = strftime('%H:%M:%S')
+        print('[{}] [{} - {}]:  {}'.format(time, self.app_name, type, message))
+
+    def run(self):
+        while True:
+            try:
+                server_text = self.read_from_server()
+                self.handle_text(server_text)
+            except (KeyboardInterrupt, SystemExit):
+                self.log('Shutting down PMCSW.', SHUTDOWN)
+                self.proc.kill()
+                sys.exit(0)
+
+if __name__ == '__main__':
+    w = Wrapper()
+    w.run()
