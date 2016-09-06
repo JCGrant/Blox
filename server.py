51e90f10bf8c89165214b05dc2d41bda30a10242
JCGrant <jamescolin.grant@gmail.com>
2016-09-06 22:24:09 +0100

    Refactor into smaller functions
    Increase request_queue_size to 5

--- server.py
+++ server.py
@@ -1,14 +1,9 @@
 import socket
 
-HOST, PORT = '', 8888
+SERVER_ADDRESS = (HOST, PORT) = ('', 8888)
+REQUEST_QUEUE_SIZE = 5
 
-listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-listen_socket.bind((HOST, PORT))
-listen_socket.listen(1)
-print('Serving HTTP on port %s ...' % PORT)
-while True:
-    client_connection, client_address = listen_socket.accept()
+def handlere_request(client_connection):
     request = client_connection.recv(1024)
     print(request)
 
@@ -18,4 +13,17 @@ HTTP/1.1 200 OK
 Hello, World!
 """
     client_connection.sendall(http_response)
-    client_connection.close()
+
+def serve_forever():
+    listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    listen_socket.bind(SERVER_ADDRESS)
+    listen_socket.listen(REQUEST_QUEUE_SIZE)
+    print('Serving HTTP on port %s ...' % PORT)
+    while True:
+        client_connection, client_address = listen_socket.accept()
+        handlere_request(client_connection)
+        client_connection.close()
+
+if __name__ == '__main__':
+    serve_forever()
